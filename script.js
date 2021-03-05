269b748ee199063e2fe8c66357223a76152165e5
unknown <bakewelljake@gmail.com>
2021-03-04 21:47:20 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+$('#img_one').click(function () {
+    $(this).fadeOut();
+});
+
+$('#img_two').click(function () {
+    $(this).fadeOut();
+});
+
+$('#img_three').click(function () {
+    $(this).fadeOut();
+});
+
+$('#img_four').click(function () {
+    $(this).fadeOut();
+});
+
+$('#img_five').click(function () {
+    $(this).fadeOut();
+});
+
+$('#img_six').click(function () {
+    $(this).fadeOut();
+});
+
+$('#img_seven').click(function () {
+    $(this).fadeOut();
+});
+
+$('#img_eight').click(function () {
+    $(this).fadeOut();
+});
+
+$('#kazuma_images button').click(function () {
+    $('#img_one').show();
+    $('#img_two').show();
+    $('#img_three').show();
+    $('#img_four').show();
+    $('#img_five').show();
+    $('#img_six').show();
+    $('#img_seven').show();
+    $('#img_eight').show();
+});
